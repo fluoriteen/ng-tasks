27f990ac7992316b42d917f4b164399803cd71f0
Ivanna Kolisnyk <ikolisnyk@cogniance.com>
2019-01-27 23:08:13 +0200

    Tour of heroes, heroes list, select hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [
+    './app.component.scss'
+  ]
 })
 export class AppComponent {
-  title = 'ng-tasks';
+  title = 'Tour of Heroes';
 }
